c953614f980a95781b4569fc8767cb17f5eb5eed
MatiJens <mateuszjens14@gmail.com>
2025-03-28 09:58:58 +0100

    metoda pokazywanie ofert

--- app/src/main/java/Classes/Buyer.kt
+++ app/src/main/java/Classes/Buyer.kt
@@ -8,6 +8,19 @@ class Buyer (
 ) : User(id,login,email,date) {
 
     fun showOffers() {
-
+        if(Offers.offers.isEmpty()) { // sprawdza czy lista nie jest pusta
+            println("Nie ma żadnych ofert")
+        }
+        else {
+            println("Oferty na rynku:")
+                    Offers.offers.forEachIndexed { index, product -> // dla kazdej
+                        print("${index+1} |")
+                        print("nazwa: ${product.name} |")
+                        print("cena: ${product.price} |")
+                        print("ilosc: ${product.quantity} |")
+                        print("opis: ${product.description} |")
+                        print("sprzedawca: ${product.seller} |")
+                    }
+        }
     }
 }
